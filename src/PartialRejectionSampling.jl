c8344d0d86b066699b3c33b44e6a2135ef9ce1e6
Guillaume Gautier <guillaume.gga@gmail.com>
2020-11-17 10:03:13 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/PartialRejectionSampling.jl
@@ -0,0 +1,5 @@
+module PartialRejectionSampling
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PartialRejectionSampling
+using Test
+
+@testset "PartialRejectionSampling.jl" begin
+    # Write your tests here.
+end
