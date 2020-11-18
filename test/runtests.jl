f5da52c554cc3326e824bd5958075a4ca959383e
Guillaume Gautier <guillaume.gga@gmail.com>
2020-11-18 10:47:55 +0100

    add script to play with ising

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
-using PartialRejectionSampling
 using Test
+import PartialRejectionSampling
+const PRS = PartialRejectionSampling
+
+# using Documenter
+# doctest(PartialRejectionSampling, fix=true)
 
 @testset "PartialRejectionSampling.jl" begin
     # Write your tests here.
